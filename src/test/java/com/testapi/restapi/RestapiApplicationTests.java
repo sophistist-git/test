1f49614467ddf7600fde4af7ee1a44c93ffe92e1
sophistist <kmichaeljy@gmail.com>
2020-12-26 12:34:14 +0900

    init

---
+++ src/test/java/com/testapi/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testapi.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
